5f7dae77fb49d3ea9cf63ac0829c742cc3180d0a
VaniaGalimzianov <vaniagalimzianov@gmail.com>
2024-06-11 13:00:29 +0300

    lang button added

---
+++ app.js
@@ -0,0 +1,25 @@
+import { langArr } from './lang.js';
+
+
+const langSwitch = document.querySelector(".lang-switch");
+
+langSwitch.addEventListener('click', function() {
+
+    if (langSwitch.checked) {
+        for (let key in langArr) {
+            try {
+                document.querySelector('.' +  key).innerHTML = langArr[key]["ru"];
+            } catch {
+                console.log("Элемент " + key + " не найден");
+            }
+        }
+    } else {
+        for (let key in langArr) {
+            try {
+                document.querySelector('.' +  key).innerHTML = langArr[key]["en"];
+            } catch {
+                console.log("Элемент " + key + " не найден");
+            }
+        }
+    }
+});
---
+++ lang.js
@@ -0,0 +1,184 @@
+const langArr = {
+    "lng-link-about": {
+        "ru": "О нас",
+        "en": "About",
+    },
+    "lng-link-faq": {
+        "ru": "Обсуждения",
+        "en": "Faq",
+    },
+    "lng-link-contacts": {
+        "ru": "Контакты",
+        "en": "Contacts",
+    },
+    "lng-success-alert": {
+        "ru": "Вы добавлены в лист ожидания",
+        "en": "You have been added to the waiting-list",
+    },
+    "lng-error-alert": {
+        "ru": "Пожалуйста, введите правильный адрес электронной почты",
+        "en": "Please enter the correct email address",
+    },
+    "lng-description": {
+        "ru": "Простой сервис для безопасной и быстрой доставки ваших посылок обычными людьми по всему миру",
+        "en": "Simple service to deliver your packages safely and fast by ordinary people around the world",
+    },
+    "lng-waitlist-text": {
+        "ru": "Присоединяйтесь к списку ожидания!",
+        "en": "Join waitlist now!",
+    },
+    "lng-join-button": {
+        "ru": "Присоединиться",
+        "en": "Join waitlist",
+    },
+    "lng-development-text": {
+        "ru": "Сервис находится в разработке. Введите свой адрес электронной почты, чтобы мы могли уведомить вас, когда он будет готов🚀",
+        "en": "Service is under development now. Enter your email address, so we can notify you when it will be ready🚀",
+    },
+    "lng-system": {
+        "ru": "How does it work?",
+        "en": "Как это работает?",
+    },
+    "lng-package-title": {
+        "ru": "Нужно доставить посылку?",
+        "en": "Need deliver package?",
+    },
+    "lng-description-package": {
+        "ru": "Вам нужно отправить посылку в другую страну? Просто найдите тех, кто летает в нужном вам направлении, и отправьте через них",
+        "en": "Need to send a gift or perhaps documents to another country? Just find people who fly in the direction you want and send it through them",
+    },
+    "lng-courier-title": {
+        "ru": "Курьером?",
+        "en": "Courier?",
+    },
+    "lng-description-courier": {
+        "ru": "Собираетесь в путешествие и у вас есть немного свободного времени и места в багаже? Помогайте людям и зарабатывайте деньги",
+        "en": "Going on a trip and have some free time and space in your luggage? Help people and earn some money",
+    },
+    "lng-first-item-h": {
+        "ru": "Найдите подходящего курьера",
+        "en": "Find suitable courier",
+    },
+    "lng-first-item-p": {
+        "ru": "Введите начальный и конечный пункты. Затем выберите объявление с подходящей датой",
+        "en": "Enter start and destination. Then select display with suitable date",
+    },
+    "lng-third-item-h": {
+        "ru": "Оплатите заказ",
+        "en": "Pay the order",
+    },
+    "lng-third-item-p": {
+        "ru": "Дождитесь, пока курьер примет ваш запрос, и оплатите его. Мы будем хранить деньги до тех пор, пока вы не получите посылку",
+        "en": "Wait until courier accepts your request and pay for it. We will keep money safe until you successfully receive your package",
+    },
+    "lng-fifth-item-h": {
+        "ru": "Передайте посылку",
+        "en": "Pass the parcel",
+    },
+    "lng-fifth-item-p": {
+        "ru": "Передайте посылку в согласованное с курьером место и время",
+        "en": "Hand over the parcel at the place and time agreed with the courier",
+    },
+    "lng-second-item-h": {
+        "ru": "Создайте запрос на доставку",
+        "en": "Create delivery request",
+    },
+    "lng-second-item-p": {
+        "ru": "Опишите, что вы хотите отправить. Прикрепите фотографию и добавьте контактные данные отправителя и получателя",
+        "en": "Describe what you want to send. Attach a photo of it and add the contact details of the sender and recipient",
+    },
+    "lng-forth-item-h": {
+        "ru": "Свяжитесь с курьером",
+        "en": "Contact the courier",
+    },
+    "lng-forth-item-p": {
+        "ru": "Свяжитесь с курьером через приложение и согласуйте детали",
+        "en": "Contact courier via application and agree details",
+    },
+    "lng-sixth-item-h": {
+        "ru": "Получите посылку",
+        "en": "Receive the parcel",
+    },
+    "lng-sixth-item-p": {
+        "ru": "Получите посылку в месте и времени, согласованном с курьером. Подтвердите получение посылки в приложении, чтобы мы могли перевести деньги курьеру",
+        "en": "Receive the parcel at the location and time agreed with the courier. Confirm receipt of parcel in the app so we can transfer money to courier",
+    },
+    "lng-faq-title": {
+        "ru": "Часто задаваемые вопросы",
+        "en": "Frequently asked questions",
+    },
+    "lng-first-faq-h": {
+        "ru": "Что делать, если мою посылку украли?",
+        "en": "What if my package will be stolen?",
+    },
+    "lng-first-faq-p": {
+        "ru": "Не волнуйтесь, по крайней мере, у вас есть жизнь. Деньги - это ничто",
+        "en": "Don't worry, at least you have your life. Money is nothing",
+    },
+    "lng-second-faq-h": {
+        "ru": "Что делать, если мою посылку украли?",
+        "en": "What if my package will be stolen?",
+    },
+    "lng-second-faq-p": {
+        "ru": "Не волнуйтесь, по крайней мере, у вас есть жизнь. Деньги - это ничто",
+        "en": "Don't worry, at least you have your life. Money is nothing",
+    },
+    "lng-third-faq-h": {
+        "ru": "Что делать, если мою посылку украли?",
+        "en": "What if my package will be stolen?",
+    },
+    "lng-third-faq-p": {
+        "ru": "Не волнуйтесь, по крайней мере, у вас есть жизнь. Деньги - это ничто",
+        "en": "Don't worry, at least you have your life. Money is nothing",
+    },
+    "lng-forth-faq-h": {
+        "ru": "Что делать, если мою посылку украли?",
+        "en": "What if my package will be stolen?",
+    },
+    "lng-forth-faq-p": {
+        "ru": "Не волнуйтесь, по крайней мере, у вас есть жизнь. Деньги - это ничто",
+        "en": "Don't worry, at least you have your life. Money is nothing",
+    },
+    "lng-fifth-faq-h": {
+        "ru": "Что делать, если мою посылку украли?",
+        "en": "What if my package will be stolen?",
+    },
+    "lng-fifth-faq-p": {
+        "ru": "Не волнуйтесь, по крайней мере, у вас есть жизнь. Деньги - это ничто",
+        "en": "Don't worry, at least you have your life. Money is nothing",
+    },
+    "lng-sixth-faq-h": {
+        "ru": "Что делать, если мою посылку украли?",
+        "en": "What if my package will be stolen?",
+    },
+    "lng-sixth-faq-p": {
+        "ru": "Не волнуйтесь, по крайней мере, у вас есть жизнь. Деньги - это ничто",
+        "en": "Don't worry, at least you have your life. Money is nothing",
+    },
+    "lng-main-heading-bottom": {
+        "ru": "Регистрируйтесь и получайте уведомления первыми!",
+        "en": "Register today and get notified first!",
+    },
+    "lng-copyright": {
+        "ru": "©2023 Handovering. Все права защищены.",
+        "en": "©2023 Handovering. All rights reserved.",
+    },
+    "lng-description-bottom": {
+        "ru": "Простой сервис для безопасной и быстрой доставки ваших посылок обычными людьми по всему миру",
+        "en": "Simple service to deliver your packages safely and fast by ordinary people around the world",
+    },
+    "lng-waitlist-text-bottom": {
+        "ru": "Присоединяйтесь к списку ожидания!",
+        "en": "Join waitlist now!",
+    },
+    "lng-join-button-bottom": {
+        "ru": "Присоединиться",
+        "en": "Join waitlist",
+    },
+    "lng-development-text-bottom": {
+        "ru": "Сервис находится в разработке. Введите свой адрес электронной почты, чтобы мы могли уведомить вас, когда он будет готов🚀",
+        "en": "Service is under development now. Enter your email address, so we can notify you when it will be ready 🚀",
+    },
+};
+
+export { langArr };
